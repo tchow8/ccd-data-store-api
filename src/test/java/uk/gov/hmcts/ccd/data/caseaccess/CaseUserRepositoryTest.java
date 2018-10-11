be25d0a759acf1b24d65861483aa44808b5e73ea
Valentin Laurin <me@vlaurin.eu>
2018-10-11 09:54:06 +0100

    RDM-3103: Test user grant/revoke audit

--- src/test/java/uk/gov/hmcts/ccd/data/caseaccess/CaseUserRepositoryTest.java
+++ src/test/java/uk/gov/hmcts/ccd/data/caseaccess/CaseUserRepositoryTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.verify;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.jdbc.core.JdbcTemplate;
